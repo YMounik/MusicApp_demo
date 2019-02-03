9a5a566b17a8cb34b16d5964c713a05d2bf9ae44
kiran kumar <kirankdev07@gmail.com>
2019-02-03 09:49:19 -0600

    Initial Commit

---
+++ MusicReleases/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicReleases
+//
+//  Created by Kirankumar Bollem on 2/3/19.
+//  Copyright © 2019 Kirankumar Bollem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
